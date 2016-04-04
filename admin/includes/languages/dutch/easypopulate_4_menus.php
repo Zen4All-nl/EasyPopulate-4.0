85dc5cd7fc004ed24904b4b5874992575da631e3
Zen4All <info@zen4all.nl>
2016-04-04 16:00:07 +0200

    translation started

--- admin/includes/languages/dutch/easypopulate_4_menus.php
+++ admin/includes/languages/dutch/easypopulate_4_menus.php
@@ -4,17 +4,17 @@
 // $display_output defines
 
 // file uploads display - output via $display_output
-define('EASYPOPULATE_4_DISPLAY_MANUFACTURERS', 'Manufacturers');
+define('EASYPOPULATE_4_DISPLAY_MANUFACTURERS', 'Fabrikanten');
 define('EASYPOPULATE_4_DISPLAY_FILTERABLE_EXPORTS', 'Filterable Exports:');
-define('EASYPOPULATE_4_DISPLAY_PRODUCTS_PRICE_EXPORT_OPTION', 'Product &amp; Pricing Export/Import Options:');
-define('EASYPOPULATE_4_DISPLAY_COMPLETE_PRODUCTS', '<b>Complete Products</b> (with Metatags)');
-define('EASYPOPULATE_4_DISPLAY_PRICE_QTY', '<b>Model/Price/Qty</b> (with Specials)');
-define('EASYPOPULATE_4_DISPLAY_PRICE_BREAKS', '<b>Model/Price/Breaks</b>');
+define('EASYPOPULATE_4_DISPLAY_PRODUCTS_PRICE_EXPORT_OPTION', 'Product &amp; Prijs Export/Import Opties:');
+define('EASYPOPULATE_4_DISPLAY_COMPLETE_PRODUCTS', '<b>Complete Producten</b> (met Metatags)');
+define('EASYPOPULATE_4_DISPLAY_PRICE_QTY', '<b>Model/Prijs/Aantsl</b> (met Aanbiedngen)');
+define('EASYPOPULATE_4_DISPLAY_PRICE_BREAKS', '<b>Model/Prijs/Breaks</b>');
 define('EASYPOPULATE_4_DISPLAY_FEATURED', '<b>Featured Products</b>');
-define('EASYPOPULATE_4_DISPLAY_TITLE_CATEGORY','Category Export/Import Options');
-define('EASYPOPULATE_4_DISPLAY_EXPORT_CATEGORY','<b>Model/Category</b>');
-define('EASYPOPULATE_4_DISPLAY_EXPORT_CATEGORYMETA','<b>Categories Only</b> (with Metatags)');
-define('EASYPOPULATE_4_DISPLAY_TITLE_ATTRIBUTE','<b>Attribute Export/Import Options</b>');
+define('EASYPOPULATE_4_DISPLAY_TITLE_CATEGORY','Categorie Export/Import Opties');
+define('EASYPOPULATE_4_DISPLAY_EXPORT_CATEGORY','<b>Model/Categorie</b>');
+define('EASYPOPULATE_4_DISPLAY_EXPORT_CATEGORYMETA','<b>Alleen categorieën</b> (met Metatags)');
+define('EASYPOPULATE_4_DISPLAY_TITLE_ATTRIBUTE','<b>Attributen Export/Import Opties</b>');
 define('EASYPOPULATE_4_DISPLAY_EXPORT_ATTRIBUTE_BASIC','<b>Basic Products Attributes</b> (basic single-line)');
 define('EASYPOPULATE_4_DISPLAY_EXPORT_ATTRIBUTE_DETAILED','<b>Detailed Products Attributes</b> (detailed multi-line)');
 define('EASYPOPULATE_4_DISPLAY_EXPORT_DETAILED_SBA', '<b>Detailed Stock By Attributes Data</b> (detailed multi-line)');
